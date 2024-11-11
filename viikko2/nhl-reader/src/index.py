a1ac3a5568fa80b260eea6007388003ee3ee5a32
Jasse Merivirta <jayzstep@gmail.com>
2024-11-11 10:04:17 +0200

    tehtävä 13 NHL pientä fiksailua

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -17,6 +17,7 @@ def main():
         "2023-24",
         "2024-25",
     ]
+    console.print("[italic]NHL statistics by nationality[/italic]")
     season = console.input(f"Select season [bold red]{'/'.join(seasons)}[/]:")
     if season in seasons:
         url = f"https://studies.cs.helsinki.fi/nhlstats/{season}/players"
@@ -32,7 +33,7 @@ def main():
         nationality = console.input(f"Select nationality: [bold red]{'/'.join(nationalities)}[/] ('exit' to quit): ")
         if nationality == "exit":
             break
-        table = Table(show_header=True, header_style="bold magenta")
+        table = Table(title=f'Top players for {nationality} in season {season}', show_header=True, header_style="bold magenta")
         table.add_column("Name")
         table.add_column("Team")
         table.add_column("Goals")

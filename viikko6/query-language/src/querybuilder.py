e4de7329b758b0f3135970e8994555cff9f50506
Jasse <jasse.merivirta@helsinki.fi>
2024-12-08 21:54:45 +0200

    viikko 6 tehtävä 5

--- viikko6/query-language/src/querybuilder.py
+++ viikko6/query-language/src/querybuilder.py
@@ -1,4 +1,4 @@
-from matchers import All, And, PlaysIn, HasAtLeast, HasFewerThan
+from matchers import All, And, PlaysIn, HasAtLeast, HasFewerThan, Or
 
 
 class QueryBuilder:
@@ -13,6 +13,9 @@ class QueryBuilder:
 
     def has_fewer_than(self, value, attr):
         return QueryBuilder(And(self.query_object, HasFewerThan(value, attr)))
+
+    def one_of(self, query1, query2):
+        return QueryBuilder(And(self.query_object, Or(query1, query2)))
 
     def build(self):
         return self.query_object
